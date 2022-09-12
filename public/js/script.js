9a9f3faedfd41426993045a28f21bcaa54930297
Johanna <j.kuhlee@gmx.de>
2022-09-12 11:14:08 +0200

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("dia-sprint JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/dia-sprint";
+
+module.exports = MONGO_URI;
